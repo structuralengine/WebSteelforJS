adca8de75b734160d90ec92aafc302e8cde6e314
mizutani488 <kmizutani710@gmail.com>
2022-02-17 10:47:18 +0900

    関数名変更

--- src/app/calculation/shape-data/set-box.service.ts
+++ src/app/calculation/shape-data/set-box.service.ts
@@ -201,7 +201,7 @@ export class SetBoxService {
     // const stl: any = this.steel.getCalcData(index); // 鉄骨
     let stl = {};
     const steelData = this.steel.getSteelJson(member.g_no);
-    for ( let n = 0; n < steelData.length; n++ ) {
+    for (let n = 0; n < steelData.length; n++) {
       const row = steelData[n];
       if (row.index === undefined) continue;
       if (row.index === index) {
@@ -241,12 +241,12 @@ export class SetBoxService {
     }
     const vertices = this.getVertices_box(vertexlist);
     const vertices_aaa = this.getVertices_box(vertexlist);
-    const param = this.getSectionParam(vertices)
+    const param = this.getSectionParam(vertices);
 
     // steel
     const steel = {
       A: null,
-      Ix: null, 
+      Ix: null,
       Iy: null,
       rs: null,
     };
@@ -255,14 +255,14 @@ export class SetBoxService {
       if (n !== null) {
         steel[n] = {
           title: stl[num].title,
-          steel_b: vertexlist['steel_b' + String(n)],
-          steel_h: vertexlist['steel_h' + String(n)],
-          steel_w: vertexlist['steel_w' + String(n)],
-          lib_b: vertexlist['lib_b' + String(n)],
-          lib_h: vertexlist['lib_h' + String(n)],
-          lib_w: vertexlist['lib_w' + String(n)],
-          lib_n: vertexlist['lib_n' + String(n)],
-          fsy: 235,// 235ではなく厚さに応じた鉄骨強度
+          steel_b: vertexlist["steel_b" + String(n)],
+          steel_h: vertexlist["steel_h" + String(n)],
+          steel_w: vertexlist["steel_w" + String(n)],
+          lib_b: vertexlist["lib_b" + String(n)],
+          lib_h: vertexlist["lib_h" + String(n)],
+          lib_w: vertexlist["lib_w" + String(n)],
+          lib_n: vertexlist["lib_n" + String(n)],
+          fsy: 235, // 235ではなく厚さに応じた鉄骨強度
         };
       }
     }
@@ -282,10 +282,10 @@ export class SetBoxService {
         steel0["fsy"] = 235; // 鉄骨幅は部材ナンバーごとに異なるため、一旦保留
       }
     } */
-    steel['A']  = param.A; 
-    steel['Ix'] = param.Ix; 
-    steel['Iy'] = param.Iy;
-    steel['rs'] = safety.safety_factor.S_rs;
+    steel["A"] = param.A;
+    steel["Ix"] = param.Ix;
+    steel["Iy"] = param.Iy;
+    steel["rs"] = safety.safety_factor.S_rs;
 
     result["steel"] = steel;
 
@@ -899,7 +899,7 @@ export class SetBoxService {
 
       θ = 0;
       for (let i = 1; i <= 5; i++) {
-        vertices.push(this.steel_vertice(element, i, θ, φ1, φ2));
+        vertices.push(this.box_vertice(element, i, θ, φ1, φ2));
       }
     } else {
       if (element["steel_w4"] === 0) {
@@ -907,7 +907,7 @@ export class SetBoxService {
       }
 
       for (let i = 1; i <= 4; i++) {
-        vertices.push(this.steel_vertice(element, i, θ, φ1, φ2));
+        vertices.push(this.box_vertice(element, i, θ, φ1, φ2));
       }
     }
 
@@ -928,10 +928,18 @@ export class SetBoxService {
       const ac = abacad.ac;
       const ad = abacad.ad;
       // 面積が0になるのでreturn
-      if ((ab.x === 0 && ab.y === 0 && ab.z === 0) && 
-          (ac.x === 0 && ac.y === 0 && ac.z === 0) && 
-          (ad.x === 0 && ad.y === 0 && ad.z === 0)   ) {
-        return new THREE.Vector3(0, 0, 0)
+      if (
+        ab.x === 0 &&
+        ab.y === 0 &&
+        ab.z === 0 &&
+        ac.x === 0 &&
+        ac.y === 0 &&
+        ac.z === 0 &&
+        ad.x === 0 &&
+        ad.y === 0 &&
+        ad.z === 0
+      ) {
+        return new THREE.Vector3(0, 0, 0);
       }
       // meshの三角形Aの重心（centroid1）と、面積（area1）をベクトルから算出
       const centroid1 = new THREE.Vector3(
@@ -989,10 +997,18 @@ export class SetBoxService {
       const ac = abacad.ac;
       const ad = abacad.ad;
       // 面積が0になるのでreturn
-      if ((ab.x === 0 && ab.y === 0 && ab.z === 0) && 
-          (ac.x === 0 && ac.y === 0 && ac.z === 0) && 
-          (ad.x === 0 && ad.y === 0 && ad.z === 0)   ) {
-        return {A, Ix, Iy}
+      if (
+        ab.x === 0 &&
+        ab.y === 0 &&
+        ab.z === 0 &&
+        ac.x === 0 &&
+        ac.y === 0 &&
+        ac.z === 0 &&
+        ad.x === 0 &&
+        ad.y === 0 &&
+        ad.z === 0
+      ) {
+        return { A, Ix, Iy };
       }
       // meshの三角形Aの重心（centroid1）と、面積（area1）をベクトルから算出
       const centroid1 = new THREE.Vector3(
@@ -1020,25 +1036,27 @@ export class SetBoxService {
         2;
       // 断面二次モーメント(Ix0, Iy0, Ax^2, Ay^2)を算出
       const nodes = [new THREE.Vector3(0, 0, 0), ab, ac, ad];
-      const Ix0 = this.getI0(nodes, 'x');
-      const Iy0 = this.getI0(nodes, 'y');
+      const Ix0 = this.getI0(nodes, "x");
+      const Iy0 = this.getI0(nodes, "y");
       const Centroid_rect = new THREE.Vector3(
         (area1 * centroid1.x + area2 * centroid2.x) / (area1 + area2),
         (area1 * centroid1.y + area2 * centroid2.y) / (area1 + area2),
         (area1 * centroid1.z + area2 * centroid2.z) / (area1 + area2)
-      )
-      const Ax2 = (area1 + area2) * (centroid.y - (position.y + Centroid_rect.y)) ** 2
-      const Ay2 = (area1 + area2) * (centroid.x - (position.x + Centroid_rect.x)) ** 2
-      // 情報を加算する 
-      A += ( area1 + area2 );
-      Ix += ( Ix0 + Ax2 );
-      Iy += ( Iy0 + Ay2 );
+      );
+      const Ax2 =
+        (area1 + area2) * (centroid.y - (position.y + Centroid_rect.y)) ** 2;
+      const Ay2 =
+        (area1 + area2) * (centroid.x - (position.x + Centroid_rect.x)) ** 2;
+      // 情報を加算する
+      A += area1 + area2;
+      Ix += Ix0 + Ax2;
+      Iy += Iy0 + Ay2;
     }
-    return {A, Ix, Iy}
+    return { A, Ix, Iy };
   }
 
   // ベクトルAB（ab）とベクトルAC（ac）とベクトルAD（ad）
-  private getAbAcAd (vertice) {
+  private getAbAcAd(vertice) {
     const ab = new THREE.Vector3(
       vertice[1].x - vertice[0].x,
       vertice[1].y - vertice[0].y,
@@ -1054,69 +1072,90 @@ export class SetBoxService {
       vertice[3].y - vertice[0].y,
       vertice[3].z - vertice[0].z
     );
-    return {ab, ac, ad}
+    return { ab, ac, ad };
   }
 
   // 三角形の断面二次モーメント
-  private getI0 (nodes, key: string = 'x'): number {
+  private getI0(nodes, key: string = "x"): number {
     let I = 0;
     // 最初に、全ての点を第一象限に格納する
     const min_x = Math.min(nodes[0].x, nodes[1].x, nodes[2].x, nodes[3].x);
     const min_y = Math.min(nodes[0].y, nodes[1].y, nodes[2].y, nodes[3].y);
     // x, yはここで分岐
-    const newNodes = []
-    if (key === 'x') {
-      newNodes.push(new THREE.Vector3(nodes[0].x - min_x, nodes[0].y - min_y, nodes[0].z));
-      newNodes.push(new THREE.Vector3(nodes[1].x - min_x, nodes[1].y - min_y, nodes[1].z));
-      newNodes.push(new THREE.Vector3(nodes[2].x - min_x, nodes[2].y - min_y, nodes[2].z));
-      newNodes.push(new THREE.Vector3(nodes[3].x - min_x, nodes[3].y - min_y, nodes[3].z));
-      newNodes.push(new THREE.Vector3(nodes[0].x - min_x, nodes[0].y - min_y, nodes[0].z));
+    const newNodes = [];
+    if (key === "x") {
+      newNodes.push(
+        new THREE.Vector3(nodes[0].x - min_x, nodes[0].y - min_y, nodes[0].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[1].x - min_x, nodes[1].y - min_y, nodes[1].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[2].x - min_x, nodes[2].y - min_y, nodes[2].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[3].x - min_x, nodes[3].y - min_y, nodes[3].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[0].x - min_x, nodes[0].y - min_y, nodes[0].z)
+      );
     } else {
-      newNodes.push(new THREE.Vector3(nodes[0].y - min_y, nodes[0].x - min_x, nodes[0].z));
-      newNodes.push(new THREE.Vector3(nodes[1].y - min_y, nodes[1].x - min_x, nodes[1].z));
-      newNodes.push(new THREE.Vector3(nodes[2].y - min_y, nodes[2].x - min_x, nodes[2].z));
-      newNodes.push(new THREE.Vector3(nodes[3].y - min_y, nodes[3].x - min_x, nodes[3].z));
-      newNodes.push(new THREE.Vector3(nodes[0].y - min_y, nodes[0].x - min_x, nodes[0].z));
+      newNodes.push(
+        new THREE.Vector3(nodes[0].y - min_y, nodes[0].x - min_x, nodes[0].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[1].y - min_y, nodes[1].x - min_x, nodes[1].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[2].y - min_y, nodes[2].x - min_x, nodes[2].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[3].y - min_y, nodes[3].x - min_x, nodes[3].z)
+      );
+      newNodes.push(
+        new THREE.Vector3(nodes[0].y - min_y, nodes[0].x - min_x, nodes[0].z)
+      );
     }
-    const X1X2X0ab = []
+    const X1X2X0ab = [];
     for (let n = 0; n < newNodes.length - 1; n++) {
-      X1X2X0ab[n] = {}
-      X1X2X0ab[n]['x1'] = newNodes[n].x;
-      X1X2X0ab[n]['x2'] = newNodes[n + 1].x;
+      X1X2X0ab[n] = {};
+      X1X2X0ab[n]["x1"] = newNodes[n].x;
+      X1X2X0ab[n]["x2"] = newNodes[n + 1].x;
       // 2点が軸に垂直
       if (newNodes[n].x - newNodes[n + 1].x === 0) {
-        X1X2X0ab[n]['a'] = 0;
-        X1X2X0ab[n]['b'] = 0;
-        X1X2X0ab[n]['x0'] = 0;
-      // 2点が軸に垂直でなく、傾きを持つ
+        X1X2X0ab[n]["a"] = 0;
+        X1X2X0ab[n]["b"] = 0;
+        X1X2X0ab[n]["x0"] = 0;
+        // 2点が軸に垂直でなく、傾きを持つ
       } else {
-        const a = (newNodes[n].y - newNodes[n + 1].y) / (newNodes[n].x - newNodes[n + 1].x);
+        const a =
+          (newNodes[n].y - newNodes[n + 1].y) /
+          (newNodes[n].x - newNodes[n + 1].x);
         const b = newNodes[n].y - a * newNodes[n].x;
-        X1X2X0ab[n]['a'] = a;
-        X1X2X0ab[n]['b'] = b;
-        X1X2X0ab[n]['x0'] = (a === 0) ? 0: (-1) * b / a;
+        X1X2X0ab[n]["a"] = a;
+        X1X2X0ab[n]["b"] = b;
+        X1X2X0ab[n]["x0"] = a === 0 ? 0 : (-1 * b) / a;
       }
-
     }
     for (const line of X1X2X0ab) {
       const x1 = line.x1;
       const x2 = line.x2;
       const x0 = line.x0;
-      const a  = line.a;
-      const b  = line.b;
+      const a = line.a;
+      const b = line.b;
 
       if (a === 0) {
-        I = (b ** 3) * (x2 - x1) / 12;
+        I = (b ** 3 * (x2 - x1)) / 12;
       } else {
-        I += ((x2 - x0) * (a*x2+b) ** 3 - (x1 - x0) * (a*x1+b) ** 3) / 36;
+        I +=
+          ((x2 - x0) * (a * x2 + b) ** 3 - (x1 - x0) * (a * x1 + b) ** 3) / 36;
       }
     }
 
-
-    return Math.abs(I)
+    return Math.abs(I);
   }
 
-  private steel_vertice(
+  private box_vertice(
     element: number,
     no: number,
     θ: number = 0,
