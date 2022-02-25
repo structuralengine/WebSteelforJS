1e2aeff668d780f29de0b802b60560aab8b820bf
Masaki-beginner <odgr-40.90-msk@outlook.jp>
2022-02-25 19:31:32 +0900

    #31 ウェブの幅厚比の照査、軸方向圧縮耐力の計算（途中）の追加。

--- src/app/calculation/result-safety-torsional-moment/calc-safety-torsional-moment.service.ts
+++ src/app/calculation/result-safety-torsional-moment/calc-safety-torsional-moment.service.ts
@@ -305,7 +305,7 @@ export class CalcSafetyTorsionalMomentService {
     }
     const Lz_b = 2 * lambda1 + 2 * lambda2;
     const Ly_b = 2 * lambda3 - tf; 
-    const lambda: number[] = [lambda1, lambda2, lambda3];
+    const lambda_list: number[] = [lambda1, lambda2, lambda3];
 
     // 断面性能
     //// 総断面
@@ -313,6 +313,7 @@ export class CalcSafetyTorsionalMomentService {
     const Ix = sectionM.steels.Ix;
     const Iy = sectionM.steels.Iy;
     const dim = sectionM.steels.dim;
+    const E: number = 2.0 * 10**5;
     const rx = ( Ix / A ) ** 0.5;
     const ry = ( Iy / A ) ** 0.5;
     //  軸方向回転時有効断面
@@ -321,9 +322,9 @@ export class CalcSafetyTorsionalMomentService {
     let centroid: THREE.Vector3;
     if (sectionM.shapeName === 'Box') {
       // 頂点座標を再取得してparamを計算
-      vertices = this.param.getVertices_fixed(sectionM, lambda)
-      centroid = this.param.getCentroid(vertices);
-      param = this.param.getSectionParam(vertices, centroid);
+      vertices = this.param.getVertices_fixed(sectionM, lambda_list);
+      // centroid = this.param.getCentroid(vertices);
+      param = this.param.getSectionParam(vertices/* , centroid */);
     }
     const yu = centroid.y;
     const Zzu = param['Ix'] / yu;
@@ -333,96 +334,66 @@ export class CalcSafetyTorsionalMomentService {
     const Zzlw = param['Ix'] / ylw;
     const yl = tension['steel_h'] + shear1['steel_h'] + compress['steel_h'] + centroid.y;
     const Zzl = param['Ix'] / yl;
+    const param_sec = {A, yu, Zzu, yuw, Zzuw, ylw, Zzlw, yl, Zzl};
 
 
     // 5.4.1 板要素の耐荷性の照査
+    const param_val = {Nd, Myd: 0, Mzd: Md};
     const sigma_N = (Nd > 0) ? 0: Nd * 1000 / A;
     const sigma_My1 = 0;
     const sigma_My2 = 0;
     const sigma_Mz1 = Md * 1000 * 1000 / Zzu;
     const sigma_Mz2 = Md * 1000 * 1000 / Zzl;
-    const sigmasigma1 = sigma_N + sigma_My1 + sigma_Mz1;
-    const sigmasigma2 = sigma_N + sigma_My2 + sigma_Mz1;
-    const sigmasigma3 = sigma_N + sigma_My1 + sigma_Mz2;
-    const sigmasigma4 = sigma_N + sigma_My2 + sigma_Mz2;
+    const sigma_list = {sigma_N, sigma_My1, sigma_My2, sigma_Mz1, sigma_Mz2};
+    // const sigmasigma1 = sigma_N + sigma_My1 + sigma_Mz1;
+    // const sigmasigma2 = sigma_N + sigma_My2 + sigma_Mz1;
+    // const sigmasigma3 = sigma_N + sigma_My1 + sigma_Mz2;
+    // const sigmasigma4 = sigma_N + sigma_My2 + sigma_Mz2;
+    // const sigmasigmas = [ [sigmasigma1, sigmasigma2], [sigmasigma3, sigmasigma4] ];
+    // const sigmasigmas1 = [sigmasigma1, sigmasigma2];
+    // const sigmasigmas2 = [sigmasigma3, sigmasigma4];
 
-    const E: number = 2.0 * 10**5;
-    const nu: number = 0.3;
+    // 
     // (1) 上フランジ
-    const bt_compress = ((compress.steel_b - shear1.steel_b) / 2) / compress.steel_h;
-    let bto_compress = 16;
-    const fsy_compress = compress.fsy.fsyk;
-    result['bt_compress'] = bt_compress;
-    result['bto_compress'] = bto_compress;
-
-    const sigma1_compress = (Math.abs(sigmasigma1) > Math.abs(sigmasigma2)) ? sigmasigma1 : sigmasigma2;
-    const sigma2_compress = (sigma1_compress === sigmasigma1) ? sigmasigma2 : sigmasigma1;
-    const psi_compress = sigma2_compress / sigma1_compress;
-    const Rcr_compress = 0.85 - 0.15 * psi_compress;
-    const k_compress = (-1.0 <= psi_compress && psi_compress < 0) ? 10*psi_compress**2 - 6.27*psi_compress + 7.63 : 8.4 / (psi_compress + 1.1);
-
-    const eta_compress = (0.85 - 0.15*psi_compress) * k_compress**0.5 / 1.4;
-    const ko_compress = 4.0;
-    const Rr_compress = 1 / eta_compress * 280 / 22 * ( (12*(1-nu**2)) / (Math.PI**2*ko_compress) * fsy_compress / E )**0.5;
-    const rho_bl_compress = 0.49 / Math.max(0.7, Rr_compress)**2;
-
-    if ( true && sigma1_compress > 0 && sigma2_compress > 0 ) {
-      bto_compress = 60;
-    } else if (sigma1_compress > 0 && sigma2_compress > 0) {
-      bto_compress = 60 * (-1);
-    } else {
-      bto_compress = Rcr_compress * ( (Math.PI**2 * k_compress) / (12*(1-nu**2)) * E / compress.fsy.fsyk  )**0.5;
-    }
-
     // (2) 下フランジ
-    const bt_tension = ((tension.steel_b - shear1.steel_b) / 2) / tension.steel_h;
-    let bto_tension = 16;
-    const fsy_tension = tension.fsy.fsyk;
-    result['bt_tension'] = bt_tension;
-    result['bto_tension'] = bto_tension;
-
-    const sigma1_tension = (Math.abs(sigmasigma3) > Math.abs(sigmasigma4)) ? sigmasigma3 : sigmasigma4;
-    const sigma2_tension = (sigma1_tension === sigmasigma3) ? sigmasigma4 : sigmasigma3;
-    const psi_tension = sigma2_tension / sigma1_tension;
-    const Rcr_tension = 0.85 - 0.15 * psi_tension;
-    const k_tension = (-1.0 <= psi_tension && psi_tension < 0) ? 10*psi_tension**2 - 6.27*psi_tension + 7.63 : 8.4 / (psi_tension + 1.1);
-
-    const eta_tension = (0.85 - 0.15*psi_tension) * k_tension**0.5 / 1.4;
-    const ko_tension = 4.0;
-    const Rr_tension = 1 / eta_tension * 280 / 22 * ( (12*(1-nu**2)) / (Math.PI**2*ko_tension) * fsy_tension / E )**0.5;
-    const rho_bl_tension = 0.49 / Math.max(0.7, Rr_tension)**2;
-
-    if ( true && sigma1_tension > 0 && sigma2_tension > 0 ) {
-      bto_tension = 60;
-    } else if (sigma1_tension > 0 && sigma2_tension > 0) {
-      bto_tension = 60 * (-1);
-    } else {
-      bto_tension = Rcr_tension * ( (Math.PI**2 * k_tension) / (12*(1-nu**2)) * E / tension.fsy.fsyk  )**0.5;
-    }
-
-    let rho_bl0: number;
-    let nu0: number;
-    let ko0: number;
-    let Rr0: number; 
-    if ( rho_bl_compress === Math.min(rho_bl_tension, rho_bl_compress) ) {
-      rho_bl0 = rho_bl_compress;
-      nu0 = eta_compress;
-      ko0 = ko_compress;
-      Rr0 = Rr_compress; 
-    } else {
-      rho_bl0 = rho_bl_tension;
-      nu0 = eta_tension;
-      ko0 = ko_tension;
-      Rr0 = Rr_tension; 
-    }
-
     // (3) 腹板
-    const dt_shear = shear1.steel_h / shear1.steel_b;
-    const dto_shear = 67.9;
-    result['dt_shear'] = dt_shear;
-    result['dto_shear'] = dto_shear;
+    const bto_list = this.calcBtoDto(tension, compress, shear1, shear2, param_sec, param_val);
+    for (const key of Object.keys(bto_list)) {
+      result[key] = bto_list[key]
+    }
 
     // 5.4.2 設計限界値の算定
+    // 軸方向圧縮耐力
+    // console.log('Lz =', Lz, ', Ly =', Ly, ', rx =', rx, ', ry =', ry);
+    const Lzrz = Lz / rx;
+    const Lyry = Ly / ry;
+    for (const flange of [compress, tension]) {
+      const key: string = (flange === compress) ? '_compress': '_tension'; 
+      // rho_bg(rho_bg_N)の計算
+      const lambda: number = (Mxd > Myd)
+                          ? 1 / Math.PI * (flange.fsy.fsyk / E)**0.5 * Lzrz
+                          : 1 / Math.PI * (flange.fsy.fsyk / E)**0.5 * Lyry;
+      let rho_bg_Nplus: number;
+      if (lambda <= 0.1) {
+        rho_bg_Nplus = 1.0;
+      } else if (0.1 < lambda && lambda <= 2**0.5) {
+        rho_bg_Nplus = 1.0 - 0.53 * (lambda - 0.1);
+      } else {
+        rho_bg_Nplus = 1.7 / (2.8 * lambda ** 2);
+      }
+      let rho_bg_Nminus: number = 1.0;
+      result['rho_bg_Nplus' + key] = rho_bg_Nplus;
+      result['rho_bg_Nminus' + key] = rho_bg_Nminus;
+
+      // rho_bl(rho_bl_N)の計算は、
+      // 幅厚比の照査で完了しているため省略.
+      const rb = (key === '_compress') ? rb_C : rb_T;
+      const fsy = flange.fsy.fsyk / rs;
+      const Nud_minus = rho_bg_Nminus * 1.0/*6ページで計算する変数*/ * A * fsy / rb_T / 1000;
+      const Nud_plus = rho_bg_Nplus * result['rho_bl' + key] * A * fsy / rb_C / 1000;
+
+    }
+
     // (1) 圧縮側
     const rho_bg: number = 1.0;
     result['rho_bg_culc'] = rho_bg;
@@ -483,6 +454,243 @@ export class CalcSafetyTorsionalMomentService {
     const ratio_MV_web_l = (ri / 1.1)**2 * ( (Md / Mud)**2 + (Vd / Vyd)**2 );
     result['ratio_MV_web_u'] = ratio_MV_web_u;
     result['ratio_MV_web_l'] = ratio_MV_web_l;
+
+    return result;
+  }
+
+  // 板要素の耐荷性の照査
+  private calcBtoDto(tension, compress, shear1, shear2, param_sec, param_val) {
+    const result = {};
+
+    // 作用応答値の集計
+    const sigma_N = param_val.Nd * 1000 / param_sec.A;
+    const sigma_N1 = (param_val.Nd > 0) ? 0 : sigma_N;
+    const sigma_My1 = 0;
+    const sigma_My2 = 0;
+    const sigma_My3 = 0;
+    const sigma_My4 = 0;
+    const sigma_Mz1 = param_val.Mzd * 1000 * 1000 / param_sec.Zzu;
+    const sigma_Mz2 = param_val.Mzd * 1000 * 1000 / param_sec.Zzuw;
+    const sigma_Mz3 = param_val.Mzd * 1000 * 1000 / param_sec.Zzlw;
+    const sigma_Mz4 = param_val.Mzd * 1000 * 1000 / param_sec.Zzl;
+    const sigmasigma1 = sigma_N1 + sigma_My1 + sigma_Mz1;
+    const sigmasigma2 = sigma_N1 + sigma_My2 + sigma_Mz1;
+    const sigmasigma3 = sigma_N1 + sigma_My1 + sigma_Mz4;
+    const sigmasigma4 = sigma_N1 + sigma_My2 + sigma_Mz4;
+    const sigmasigmas = [ [sigmasigma1, sigmasigma2], [sigmasigma3, sigmasigma4] ];
+
+    // 上フランジ（圧縮側）
+    const bto_com = this.calcBto_flange(compress, shear1, [sigmasigma1, sigmasigma2]);
+    for (const key of Object.keys(bto_com)) {
+      result[key + '_compress'] = bto_com[key];
+    }
+    let kai: any;
+    let kai_bto: any;
+    if( result['bt_compress'] < result['bto_compress'] ) {
+      kai = '---';
+      kai_bto = '---';
+    } else {
+      if (true) {
+        kai = 1.2;
+        kai_bto = kai * result['bto_compress'];
+      } else {
+        kai = 1.7;
+        kai_bto = kai * result['bto_compress'];
+      }
+    }
+    result['kai_compress'] = kai;
+    result['kai_bto_compress'] = kai_bto;
+
+    // 下フランジ（引張側）
+    const bto_ten = this.calcBto_flange(tension, shear1, [sigmasigma3, sigmasigma4]);
+    for (const key of Object.keys(bto_ten)) {
+      result[key + '_tension'] = bto_ten[key];
+    }
+    if( result['bt_tension'] < result['bto_tension'] ) {
+      kai = '---';
+      kai_bto = '---';
+    } else {
+      if (true) {
+        kai = 1.2
+        kai_bto = kai * result['bto_tension'];
+      } else {
+        kai = 1.7
+        kai_bto = kai * result['bto_tension'];
+      }
+    }
+    result['kai_tension'] = kai;
+    result['kai_bto_tension'] = kai_bto;
+
+    // 腹板（せん断）
+    const d: number = tension['steel_h'] + shear1['steel_h'] + compress['steel_h']; 
+    const sigma_d: number = Math.abs(sigma_N) + Math.abs(sigma_My3) + Math.abs(sigma_Mz3);
+    const tau_d: number = 0 / (shear1['steel_h'] * shear1['steel_b'])
+    const Dto = this.calcDto(d, shear1, shear2, 400, {sigma_d, tau_d});
+    for (const key of Object.keys(Dto)) {
+      result[key + '_shear'] = Dto[key];
+    }
+
+    // 局部座屈に対する係数の算出
+    const rho_bl_compress = result['rho_bl_compress'];
+    const rho_bl_tension = result['rho_bl_tension'];
+    const key_min: string = ( rho_bl_compress === Math.min(rho_bl_tension, rho_bl_compress) )
+                          ? '_compress'
+                          : 'tension';
+    const rho_bl0: number = result['rho_bl' + key_min];
+    const eta0: number = result['eta' + key_min];
+    const ko0: number = result['ko' + key_min];
+    const Rr0: number = result['Rr' + key_min];
+    result['rho_bl0'] = rho_bl0;
+    result['eta0'] = eta0;
+    result['ko0'] = ko0;
+    result['Rr0'] = Rr0;
+
+
+    return result;
+  }
+
+  private calcBto_flange(element, shear, sigmasigmas): any {
+    const result = {};
+
+    const E: number = 2.0 * 10**5;
+    const nu: number = 0.3;
+
+    // elementはtensionまたはcompress
+    const bt = ((element.steel_b - shear.steel_b) / 2) / element.steel_h;
+    result['bt'] = bt;
+    const fsy = element.fsy.fsyk;
+
+    const sigma1 = (Math.abs(sigmasigmas[0]) > Math.abs(sigmasigmas[1])) ? sigmasigmas[0] : sigmasigmas[1];
+    const sigma2 = (sigma1 === sigmasigmas[0]) ? sigmasigmas[1] : sigmasigmas[0];
+    const psi = sigma2 / sigma1;
+    const Rcr = 0.85 - 0.15 * psi;
+    const k = (-1.0 <= psi && psi < 0) ? 10*psi**2 - 6.27*psi + 7.63 : 8.4 / (psi + 1.1);
+
+    result['psi'] = psi;
+    result['Rcr'] = Rcr;
+    result['k'] = k;
+
+    const eta = (0.85 - 0.15*psi) * k**0.5 / 1.4;
+    const ko = 4.0;
+    const Rr = 1 / eta * 280 / 22 * ( (12*(1-nu**2)) / (Math.PI**2*ko) * fsy / E )**0.5;
+    const rho_bl = 0.49 / Math.max(0.7, Rr)**2;
+
+    let bto = 16;
+    if ( true && sigma1 > 0 && sigma2 > 0 ) {
+      bto = 60;
+    } else if (sigma1 > 0 && sigma2 > 0) {
+      bto = 60 * (-1);
+    } else {
+      bto = Rcr * ( (Math.PI**2 * k) / (12*(1-nu**2)) * E / element.fsy.fsyk  )**0.5;
+    }
+    result['bto'] = bto;
+
+    result['rho_bl'] = rho_bl;
+    result['eta'] = eta;
+    result['ko'] = ko;
+    result['Rr'] = Rr;
+
+    return result;
+  }
+
+  private calcDto (d, shear1, shear2, num, sigma_list) {
+    const result = {};
+
+    const Dw: number = shear1.steel_h;
+    const tw: number = shear1.steel_b;
+    const Dt: number = Dw / tw;
+    result['Dt'] = Dt;
+
+    let Dto: number;
+    const element_no = num; // SM400;
+    // 水平補剛版があるときはtrue, 無ければfalse
+    if (false) {
+      if (element_no === 570) {
+        Dto = 225;
+      } else {
+        Dto = 250;
+      }
+    } else {
+      if (element_no === 400) {
+        if (0 <= tw && tw <= 16) {
+          Dto = 132.8;
+        } else if (16 < tw && tw <= 40) {
+          Dto = 135.6;
+        } else {
+          Dto = 141.8;
+        }
+      } else if (element_no === 490) {
+        if (0 <= tw && tw <= 16) {
+          Dto = 115.3;
+        } else if (16 < tw && tw <= 40) {
+          Dto = 117.1;
+        } else {
+          Dto = 121.0;
+        }
+      } else if (element_no === 520) {
+        if (0 <= tw && tw <= 16) {
+          Dto = 108.8;
+        } else if (16 < tw && tw <= 40) {
+          Dto = 110.3;
+        } else {
+          Dto = 113.6;
+        }
+      } else {
+        if (0 <= tw && tw <= 16) {
+          Dto = 96.9;
+        } else if (16 < tw && tw <= 40) {
+          Dto = 98.0;
+        } else {
+          Dto = 100.2;
+        }
+      }
+    }
+    result['Dto'] = Dto;
+    const dDw: number = d / Dw;
+    
+    let Ax40;
+    if (false && dDw < 0.8) {
+      Ax40 = 0.8;
+    } else if (dDw < 1) {
+      Ax40 = 1
+    } else {
+      Ax40 = 2
+    }
+    let Ax41;
+    if (false && dDw > 0.8) {
+      Ax41 = 0.8;
+    } else if (false && dDw <= 0.8) {
+      Ax41 = '-';
+    } else if (dDw > 1) {
+      Ax41 = 1;
+    } else {
+      Ax41 = '-';
+    }
+    const AB44: number = (false) ? 2036 : 377;
+    let AM44: number;
+    let AT44: number;
+    if (false) {
+      if (dDw <= 0.8) {
+        AM44 = 98.6;
+        AT44 = 84.3;
+      } else {
+        AM44 = 132.0;
+        AT44 = 63.1;
+      }
+    } else {
+      if (dDw <= 1) {
+        AM44 = 63.1;
+        AT44 = 84.3;
+      } else {
+        AM44 = 84.3;
+        AT44 = 63.1;
+      }
+    }
+
+    const ratio: number = ( ( Dw / ( 100 * tw ) )**4)
+                        * ( ( sigma_list.sigma_d / AB44 )**2
+                        + ( sigma_list.tau_d / (AM44 + AT44 * ((1 / dDw) ** 2)))**2);
+
 
     return result;
   }
